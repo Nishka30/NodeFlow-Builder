1d98c4f187726187d969f97e583a027f6881d3be
Nishka <shrimalinishka@gmail.com>
2024-08-31 13:13:13 +0530

    new nodes creation

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,18 @@
-import { PipelineToolbar } from './toolbar';
-import { PipelineUI } from './ui';
-import { SubmitButton } from './submit';
+// frontend\src\App.js
+
+import { PipelineToolbar } from "./toolbar";
+import { PipelineUI } from "./ui";
+import { SubmitButton } from "./submit";
+import { ReactFlowProvider } from "reactflow";
 
 function App() {
   return (
     <div>
-      <PipelineToolbar />
-      <PipelineUI />
-      <SubmitButton />
+      <ReactFlowProvider>
+        <PipelineToolbar />
+        <PipelineUI />
+        <SubmitButton />
+      </ReactFlowProvider>
     </div>
   );
 }
--- frontend/src/nodes/textNode.js
+++ frontend/src/nodes/textNode.js
@@ -1,35 +1,24 @@
-// textNode.js
+// TextNode.js
 
-import { useState } from 'react';
-import { Handle, Position } from 'reactflow';
+import { AbstractNode } from "./AbstractNode";
+import { useState } from "react";
+import { Position } from "reactflow";
 
 export const TextNode = ({ id, data }) => {
-  const [currText, setCurrText] = useState(data?.text || '{{input}}');
+  const [text, setText] = useState(data?.text || "");
+  const handles = [{ type: "source", position: Position.Right, id: "output" }];
 
-  const handleTextChange = (e) => {
-    setCurrText(e.target.value);
+  const handleTextChange = (newText) => {
+    setText(newText);
   };
 
   return (
-    <div style={{width: 200, height: 80, border: '1px solid black'}}>
-      <div>
-        <span>Text</span>
-      </div>
-      <div>
-        <label>
-          Text:
-          <input 
-            type="text" 
-            value={currText} 
-            onChange={handleTextChange} 
-          />
-        </label>
-      </div>
-      <Handle
-        type="source"
-        position={Position.Right}
-        id={`${id}-output`}
-      />
-    </div>
+    <AbstractNode
+      id={id}
+      data={{ ...data, text }}
+      type="Text"
+      handles={handles}
+      onChange={handleTextChange}
+    />
   );
-}
+};
